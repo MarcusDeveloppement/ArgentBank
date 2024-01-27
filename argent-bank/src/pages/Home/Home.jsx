cb3b97d1fab0be6a28b567c2eb35c9c2e47a9a1b
Marcus <marcus.frontdev@gmail.com>
2024-01-27 17:24:02 +0100

    delete imports not used

--- argent-bank/src/pages/Home/Home.jsx
+++ argent-bank/src/pages/Home/Home.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Header from "../../components/Header/Header";
 import MainArticle from "../../components/MainArticle/MainArticle";
-import Footer from "../../components/Footer/Footer";
 
 export default function Home() {
   return (
